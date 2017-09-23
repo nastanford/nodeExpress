701e2890ea47c49634514e82e9d1f31952c5d54c
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2017-09-23 11:15:05 -0400

    Changed from Node HTTP Server to Express Server.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,27 @@
+// Express Webserver Example
+var express = require("express");
+var app = express();
+var port = 3000;
+
+app.get('/',function(req,res){
+  res.send('<h1>Stanford Meetups</h1>');
+});
+
+var server = app.listen(port,function() {
+  console.log(`Express Webserver started go to http://localhost:${port}`);
+});
+
+/* Node JS Web Server with Express
 var http = require('http');
+var port = 3000;
 
 var myServer = http.createServer(function(request, response)
 {
-  response.writeHead(200, {"Content-Type": "text/plain"})
-  response.write('Nathan\' Meetups');
+  response.writeHead(200, {"Content-Type": "text/html"})
+  response.write('<h1>Nathan\' Meetups</h1>');
   response.end();
 });
 
-myServer.listen(3000);
+myServer.listen(port);
+console.log(`Webserver started http://localhost:${port}`);
+*/
